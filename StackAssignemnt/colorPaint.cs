1ea6f6d662c84992d1c8baf76a88953e587aed10
lvashist <lvashist@LIN-PF1DM74D>
2024-06-24 23:55:15 +0530

    Add project files.

---
+++ StackAssignemnt/colorPaint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAssignemnt
+{
+    internal class colorPaint
+    {
+        public string ColorName { get; set; }
+        public string RGBValue { get; set; }
+
+        public string FinishType { get; set;    }
+        
+        public bool IsAvailable { get; set; }
+        public double PricePerGallon { get; set; }
+    }
+}
